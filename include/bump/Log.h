84af22a1f53d8cdd9091ca3688ddfec02ba50f83
Christian Noon <christian.noon@gmail.com>
2012-12-04 10:22:02 -0600

    Added a Log class to support the bump logging system.

---
+++ include/bump/Log.h
@@ -0,0 +1,280 @@
+//
+//	Log.h
+//	Bump
+//
+//	Created by Christian Noon on 12/3/12.
+//	Copyright (c) 2012 Christian Noon. All rights reserved.
+//
+
+#ifndef BUMP_LOG_H
+#define BUMP_LOG_H
+
+// Boost headers
+#include <boost/thread.hpp>
+
+// Bump headers
+#include <bump/Export.h>
+#include <bump/String.h>
+
+// Environment variable name definitions
+#define BUMP_LOG_ENABLED	"BUMP_LOG_ENABLED"
+#define BUMP_LOG_FILE		"BUMP_LOG_FILE"
+#define BUMP_LOG_LEVEL		"BUMP_LOG_LEVEL"
+
+// Defines the bump prefix for logging
+#define bumpPrefix			"[bump] "
+
+namespace bump {
+
+/** The various levels of log message verbosity. */
+enum LogLevel
+{
+	ALWAYS_LVL,
+	ERROR_LVL,
+	WARNING_LVL,
+	INFO_LVL,
+	DEBUG_LVL
+};
+
+/** The various date time formats. */
+enum DateTimeFormat
+{
+	DATE_TIME_DEFAULT,
+	DATE_TIME_WITH_AM_PM,
+	DATE_TIME_PRECISE,
+	TIME_DEFAULT,
+	TIME_WITH_AM_PM,
+	TIME_PRECISE
+};
+
+/**
+ * The Log class handles all logging.
+ *
+ * This logging system supports the following:
+ *
+ *	  - Five different log levels
+ *	  - Output redirection to files (a custom std::ofstream)
+ *	  - Thread-safe access to the stream buffer
+ *    - Date time formatting
+ *	  - Disabling the log altogether
+ *
+ * The following environment variables can be used to configure the log at runtime:
+ *
+ *	  - BUMP_LOG_ENABLED: Disables the log system if set to any of the following:
+ *		  * [ NO | FALSE | NOPE | DISABLE ]
+ *	  - BUMP_LOG_FILE: Redirects the output from the log to a specified file:
+ *		  * /home/username/output.txt
+ *	  - BUMP_LOG_LEVEL: Defines the maximum output level for the log:
+ *		  * [ ALWAYS_LVL | ERROR_LVL | WARNING_LVL | INFO_LVL | DEBUG_LVL ]
+ *
+ * Using the log is straight-forward. There are several different convenience macros
+ * provided to make this as simple as possible. The easiest way to use the log is with
+ * the following macros:
+ *
+ *    - LOG_ALWAYS()
+ *    - LOG_ERROR()
+ *    - LOG_WARNING()
+ *    - LOG_INFO()
+ *    - LOG_DEBUG()
+ *
+ * Here is a simple example using the LOG_DEBUG() macro:
+ *
+ *    - LOG_DEBUG() << "this is my output" << std::endl;
+ *
+ * Sometimes, it is useful to be able to prefix your output with something like a
+ * namespace, application name, error message, etc. For this purpose, the following
+ * macros exist:
+ *
+ *	  - LOG_ALWAYS_P(prefix)
+ *	  - LOG_ERROR_P(prefix)
+ *	  - LOG_WARNING_P(prefix)
+ *	  - LOG_INFO_P(prefix)
+ *	  - LOG_DEBUG_P(prefix)
+ *
+ * Here are some common examples of different logging messages:
+ *
+ *	  - LOG_ERROR_P("[bump] ") << "ERROR: We have a problem here" << std::endl;
+ *	  - LOG_ALWAYS_P("[bump] ") << "Initializing the logging system" << std::endl;
+ *
+ * As you can see, it is just like using std::cout, except with a bunch of extra benefits!
+ */
+class BUMP_EXPORT Log
+{
+public:
+
+	/**
+	 * Creates a singleton instance.
+	 *
+	 * @return the singleton Log instance.
+	 */
+	static Log* instance() { static Log log; return &log; }
+
+	/**
+	 * Sets whether the log is enabled.
+	 *
+	 * @param enabled whether the log is enabled.
+	 */
+	void setIsLogEnabled(bool enabled);
+
+	/**
+	 * Returns whether the log is enabled.
+	 *
+	 * @return true if the log is enabled, false otherwise.
+	 */
+	bool isLogEnabled();
+
+	/**
+	 * Determines whether the log level is enabled.
+	 *
+	 * @param logLevel the log level to determine if enabled.
+	 * @return whether the given logLevel is enabled.
+	 */
+	bool isLogLevelEnabled(LogLevel logLevel);
+
+	/**
+	 * Sets the log level.
+	 *
+	 * @param logLevel the log level to set the Log instance to.
+	 */
+	void setLogLevel(LogLevel logLevel);
+
+	/**
+	 * Determines the log level.
+	 *
+	 * @return the log level for the Log instance.
+	 */
+	LogLevel logLevel();
+
+	/**
+	 * Sets whether the date time formatting display is enabled.
+	 *
+	 * @param enabled whether the date time formatting display is enabled.
+	 */
+	void setIsDateTimeFormattingEnabled(bool enabled);
+	
+	/**
+	 * Determines whether the date time formatting display is enabled.
+	 *
+	 * @return true if the date time formatting display is enabled, false otherwise.
+	 */
+	bool isDateTimeFormattingEnabled();
+
+	/**
+	 * Sets the date time format.
+	 *
+	 * @param format the date time format to set the Log instance to.
+	 */
+	void setDateTimeFormat(const DateTimeFormat& format);
+
+	/**
+	 * Determines the date time format.
+	 *
+	 * @return the date time format.
+	 */
+	DateTimeFormat dateTimeFormat();
+
+	/**
+	 * Sets the output stream to a std::ofstream created from the provided filepath.
+	 *
+	 * NOTE: This will not change the current log stream if the file cannot be opened successfully.
+	 *
+	 * @param filepath the filepath of the file to write all the log output to.
+	 * @return true if the stream was replaced, false otherwise.
+	 */
+	bool setLogFile(const String& filepath);
+
+	/**
+	 * Switches the current output stream to the provided stream.
+	 *
+	 * @param stream the output stream to start using for the Log instance.
+	 */
+	void setLogStream(std::ostream& stream);
+
+	/**
+	 * Returns the log output stream (use like std::cout).
+	 *
+	 * @param logLevel the log level of the stream being requested.
+	 * @param prefix the string to tack onto the front of the message.
+	 * @return the output stream to write to.
+	 */
+	std::ostream& logStream(LogLevel level, const String& prefix = "");
+
+protected:
+
+	/**
+	 * Constructor.
+	 *
+	 * Initializes the log level by attempting to grab the BUMP_LOG_LEVEL environment
+	 * variables from the user's environment.
+	 */
+	Log();
+
+	/**
+	 * Destructor.
+	 */
+	~Log();
+
+public:
+
+	/**
+	 * Returns the current time as a formatted string.
+	 *
+	 * @return the current time as a formatted string.
+	 */
+	String	_convertTimeToString();
+
+	/** Instance member variables. */
+	bool					_isEnabled;
+	LogLevel				_logLevel;
+	bool					_isDateTimeFormatEnabled;
+	DateTimeFormat			_dateTimeFormat;
+	std::ostream*			_logStream;
+	boost::mutex			_mutex;
+};
+
+/**
+ * Convenience macro for accessing the Log singleton.
+ */
+#define LOG() bump::Log::instance()
+
+/**
+ * Log messages with levels. Below are a couple examples:
+ *
+ * LOG_WARNING() << "This is an example of a WARNING message" << std::endl;
+ * - Would log: "This is an example of a WARNING message"
+ *
+ * LOG_ERROR() << "ERROR: Look at what happened!" << std::endl;
+ * - Would log: "ERROR: Look at what happened!" << std::endl;
+ */
+#define LOG_ALWAYS() log(bump::ALWAYS_LVL)
+#define LOG_ERROR() log(bump::ERROR_LVL)
+#define LOG_WARNING() log(bump::WARNING_LVL)
+#define LOG_INFO() log(bump::INFO_LVL)
+#define LOG_DEBUG() log(bump::DEBUG_LVL)
+
+/**
+ * Logs a message with the defined prefix. Below are a couple examples:
+ *
+ * LOG_ERROR_P(bumpPrefix) << "ERROR: Look at what happened!" << std::endl;
+ * - Would log: "[bump] ERROR: Look at what happened!"
+ *
+ * LOG_ERROR_P(bumpPrefix) << "This output uses the preprocessor definition bumpPrefix" << std::endl;
+ * - Would log: "[bump] This output uses the preprocessor definition bumpPrefix"
+ */
+#define LOG_ALWAYS_P(prefix) logWithPrefix(bump::ALWAYS_LVL, prefix)
+#define LOG_ERROR_P(prefix) logWithPrefix(bump::ERROR_LVL, prefix)
+#define LOG_WARNING_P(prefix) logWithPrefix(bump::WARNING_LVL, prefix)
+#define LOG_INFO_P(prefix) logWithPrefix(bump::INFO_LVL, prefix)
+#define LOG_DEBUG_P(prefix) logWithPrefix(bump::DEBUG_LVL, prefix)
+
+/**
+ * DO NOT USE THESE MACROS!!!
+ *
+ * Only for internal use, please use the methods above for logging.
+ */
+#define log(level) if ((LOG()->isLogLevelEnabled(level)) && level <= LOG()->logLevel()) LOG()->logStream(level)
+#define logWithPrefix(level, prefix) if ((LOG()->isLogLevelEnabled(level)) && level <= LOG()->logLevel()) LOG()->logStream(level, prefix)
+
+}	// End of bump namespace
+
+#endif	// End of BUMP_LOG_H
---
+++ src/bump/Log.cpp
@@ -0,0 +1,251 @@
+//
+//	Log.cpp
+//	Bump
+//
+//	Created by Christian Noon on 12/3/12.
+//	Copyright (c) 2012 Christian Noon. All rights reserved.
+//
+
+// C++ headers
+#include <fstream>
+
+// Boost headers
+#include <boost/date_time/posix_time/posix_time.hpp>
+
+// Bump headers
+#include <bump/Environment.h>
+#include <bump/Log.h>
+
+using namespace bump;
+
+Log::Log() :
+	_isEnabled(true),
+	_logLevel(WARNING_LVL),
+	_isDateTimeFormatEnabled(false),
+	_dateTimeFormat(DATE_TIME_DEFAULT),
+	_logStream(&std::cout)
+{
+	// Attempt to disable the entire log system based on the "BUMP_LOG_ENABLED" environment variable
+	String logEnabled = bump::Environment::environmentVariable(BUMP_LOG_ENABLED);
+	logEnabled.toLowerCase();
+	if (logEnabled == "no" || logEnabled == "false" || logEnabled == "nope" || logEnabled == "disable")
+	{
+		_isEnabled = false;
+		std::cout << "[bump] Setting LOG_ENABLED to NO" << std::endl;
+		return;
+	}
+
+	// Attempt to set the log level based on the "BUMP_LOG_LEVEL" environment variable
+	String logLevel = bump::Environment::environmentVariable(BUMP_LOG_LEVEL);
+	if (logLevel == "ALWAYS_LVL")
+	{
+		_logLevel = ALWAYS_LVL;
+		std::cout << "[bump] Setting BUMP_LOG_LEVEL to ALWAYS" << std::endl;
+	}
+	else if (logLevel == "ERROR_LVL")
+	{
+		_logLevel = ERROR_LVL;
+		std::cout << "[bump] Setting BUMP_LOG_LEVEL to ERROR" << std::endl;
+	}
+	else if (logLevel == "WARNING_LVL")
+	{
+		_logLevel = WARNING_LVL;
+		std::cout << "[bump] Setting BUMP_LOG_LEVEL to WARNING" << std::endl;
+	}
+	else if (logLevel == "INFO_LVL")
+	{
+		_logLevel = INFO_LVL;
+		std::cout << "[bump] Setting BUMP_LOG_LEVEL to INFO" << std::endl;
+	}
+	else if (logLevel == "DEBUG_LVL")
+	{
+		_logLevel = DEBUG_LVL;
+		std::cout << "[bump] Setting BUMP_LOG_LEVEL to DEBUG" << std::endl;
+	}
+	else if (!logLevel.empty())
+	{
+		std::cout << "[bump] WARNING: Your BUMP_LOG_LEVEL environment variable: [" << logLevel
+			<< "] does not match any of the possible options: [ ALWAYS_LVL | ERROR_LVL | WARNING_LVL "
+			<< "| INFO_LVL | DEBUG_LVL ]" << std::endl;
+	}
+	else
+	{
+		// DO NOTHING
+	}
+
+	// Attempt to set the log file based on the "BUMP_LOG_FILE" environment variable
+	String logFile = bump::Environment::environmentVariable(BUMP_LOG_FILE);
+	if (!logFile.empty())
+	{
+		if (logFile == "stderr")
+		{
+			_logStream = &std::cerr;
+		}
+		else if (logFile != "stdout")
+		{
+			bool success = setLogFile(logFile);
+			if (success)
+			{
+				std::cout << "[bump] Setting BUMP_LOG_FILE to " << logFile << std::endl;
+			}
+			else
+			{
+				std::cout << "[bump] WARNING: Your BUMP_LOG_FILE environment variable: ["
+					<< logFile << "] could not be created or opened" << std::endl;
+			}
+		}
+	}
+}
+
+Log::~Log()
+{
+	;
+}
+
+void Log::setIsLogEnabled(bool enabled)
+{
+	boost::mutex::scoped_lock lock(_mutex);
+	_isEnabled = enabled;
+}
+
+bool Log::isLogEnabled()
+{
+	boost::mutex::scoped_lock lock(_mutex);
+	return _isEnabled;
+}
+
+bool Log::isLogLevelEnabled(LogLevel logLevel)
+{
+	boost::mutex::scoped_lock lock(_mutex);
+
+	// Returning false if disabled
+	if (!_isEnabled)
+	{
+		return false;
+	}
+
+	return logLevel <= _logLevel;
+}
+
+void Log::setLogLevel(LogLevel logLevel)
+{
+	boost::mutex::scoped_lock lock(_mutex);
+	_logLevel = logLevel;
+}
+
+LogLevel Log::logLevel()
+{
+	boost::mutex::scoped_lock lock(_mutex);
+	return _logLevel;
+}
+
+void Log::setDateTimeFormat(const DateTimeFormat& format)
+{
+	boost::mutex::scoped_lock lock(_mutex);
+	_dateTimeFormat = format;
+}
+
+void Log::setIsDateTimeFormattingEnabled(bool enabled)
+{
+	boost::mutex::scoped_lock lock(_mutex);
+	_isDateTimeFormatEnabled = enabled;
+}
+
+bool Log::isDateTimeFormattingEnabled()
+{
+	boost::mutex::scoped_lock lock(_mutex);
+	return _isDateTimeFormatEnabled;
+}
+
+DateTimeFormat Log::dateTimeFormat()
+{
+	boost::mutex::scoped_lock lock(_mutex);
+	return _dateTimeFormat;
+}
+
+bool Log::setLogFile(const String& filepath)
+{
+	boost::mutex::scoped_lock lock(_mutex);
+
+	// First try to open the file
+	std::ofstream* logFile = new std::ofstream(filepath.c_str());
+	if ( !(*logFile) )
+	{
+		delete logFile;
+		logFile = NULL;
+		return false;
+	}
+
+	// We successfully opened the file for writing, so switch log streams
+	_logStream = logFile;
+	return true;
+}
+
+void Log::setLogStream(std::ostream& stream)
+{
+	boost::mutex::scoped_lock lock(_mutex);
+	_logStream = &stream;
+}
+
+std::ostream& Log::logStream(LogLevel level, const String& prefix)
+{
+	boost::mutex::scoped_lock lock(_mutex);
+
+	// The generic stream to "buffer" output to for the output handler. This allows us to
+	// avoid pointer dereferences until necessary.
+	std::ostream& ostream = *_logStream;
+
+	// Append the date time if necessary
+	if (_isDateTimeFormatEnabled)
+	{
+		ostream << _convertTimeToString() << " ";
+	}
+
+	// Append the prefix if necessary
+	if (!prefix.empty())
+	{
+		ostream << prefix;
+	}
+
+	return *_logStream;
+}
+
+String Log::_convertTimeToString()
+{
+	// Create a time facet for date formatting purposes
+	boost::posix_time::time_facet* facet;
+	if (_dateTimeFormat == DATE_TIME_DEFAULT)
+	{
+		facet = new boost::posix_time::time_facet("%Y-%m-%d %l:%M:%S");
+	}
+	else if (_dateTimeFormat == DATE_TIME_WITH_AM_PM)
+	{
+		facet = new boost::posix_time::time_facet("%Y-%m-%d %l:%M:%S %p");
+	}
+	else if (_dateTimeFormat == DATE_TIME_PRECISE)
+	{
+		facet = new boost::posix_time::time_facet("%Y-%m-%d %l:%M:%s");
+	}
+	else if (_dateTimeFormat == TIME_DEFAULT)
+	{
+		facet = new boost::posix_time::time_facet("%l:%M:%S");
+	}
+	else if (_dateTimeFormat == TIME_WITH_AM_PM)
+	{
+		facet = new boost::posix_time::time_facet("%l:%M:%S %p");
+	}
+	else // _dateTimeFormat == TIME_PRECISE
+	{
+		facet = new boost::posix_time::time_facet("%l:%M:%s");
+	}
+
+	// Create an ostringstream and imbue it with the local locale and facet
+	std::ostringstream ostream;
+	ostream.imbue(std::locale(ostream.getloc(), facet));
+
+	// Push the current local time into the ostringstream and return the string version
+	boost::posix_time::ptime now = boost::posix_time::second_clock::local_time();
+	ostream << now;
+
+	return ostream.str();
+}
