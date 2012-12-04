cb876f590feb2c0401a0f5f0c17986cb4cffcc1a
Christian Noon <christian.noon@gmail.com>
2012-12-04 12:26:14 -0600

    Added the timer classes to the bump inclusive header.

--- include/bump/bump.h
+++ include/bump/bump.h
@@ -9,12 +9,14 @@
 #ifndef BUMP_BUMP_H
 #define BUMP_BUMP_H
 
+#include <bump/AutoTimer.h>
 #include <bump/Environment.h>
 #include <bump/Exception.h>
 #include <bump/Export.h>
 #include <bump/Log.h>
 #include <bump/NotificationCenter.h>
 #include <bump/String.h>
+#include <bump/Timer.h>
 #include <bump/Uuid.h>
 #include <bump/Version.h>
 
