1646bb2abac1d87bda4f998aa544ef373a5b751f
Christian Noon <christian.noon@gmail.com>
2012-12-05 11:03:29 -0600

    Added a bunch of path decomposition methods for the FileInfo class such as absolutePath, filename, extension, etc.

--- include/bump/FileInfo.h
+++ include/bump/FileInfo.h
@@ -36,11 +36,27 @@ public:
 	~FileInfo();
 
 	/**
+	 *====================================================================================
+	 *                              Path Query Methods
+	 *====================================================================================
+	 */
+
+	/**
 	 * Returns whether the path exists.
 	 *
 	 * @return true if the path exists, false otherwise.
 	 */
 	bool exists() const;
+
+	/**
+	 * Returns the file size of the path if it exists and is a file.
+	 *
+	 * Throws a bump::FileSystemError if the path does not exist.
+	 * Throws a bump::FileSystemError if the path is not a file.
+	 *
+	 * @return file size of path if exists and is file, otherwise returns -1.
+	 */
+	unsigned long long fileSize() const;
 
 	/**
 	 * Returns whether the path is an absolute path.
@@ -78,13 +94,91 @@ public:
 	bool isSymbolicLink() const;
 
 	/**
+	 * Returns whether the path points an empty directory or empty file.
+	 *
+	 * Throws bump::FileSystemError if the path does not exist.
+	 *
+	 * @return true if the path points to an empty directory or empty file, false othwerwise.
+	 */
+	bool isEmpty() const;
+
+	/**
 	 * Returns whether the path points to a hidden file.
 	 *
 	 * @return true if the path points to a hidden file, false othwerwise.
 	 */
 	bool isHidden() const;
 
+	/**
+	 *====================================================================================
+	 *                          Path Decomposition Methods
+	 *====================================================================================
+	 */
+
+	/**
+	 * Returns the absolute path without cleaning the path or resolving symbolic links.
+	 *
+	 * @return the absolute path without cleaning the path or resolving symbolic links.
+	 */
+	String absolutePath() const;
+
+	/**
+	 * Returns the absolute path by resolving symbolic links, removing dot and dot-dot elements.
+	 *
+	 * @return the cleaned absolute path.
+	 */
+	String canonicalPath() const;
+
+	/**
+	 * Returns the parent path, i.e. /home/users/user/output.txt == /home/users/user.
+	 *
+	 * @return the parent path.
+	 */
+	String parentPath() const;
+
+	/**
+	 * Returns the basename of the file, i.e. output.txt == output and output.tar.gz == output.
+	 *
+	 * @return the basename of the file.
+	 */
+	String basename() const;
+
+	/**
+	 * Returns the complete basename of the file, i.e. output.txt == output and output.tar.gz == output.tar.
+	 *
+	 * @return the complete basename of the file.
+	 */
+	String completeBasename() const;
+
+	/**
+	 * Returns the extension of the file, i.e. output.txt == txt and output.tar.gz == gz.
+	 *
+	 * @return the extension of the file.
+	 */
+	String extension() const;
+
+	/**
+	 * Returns the complete extension of the file, i.e. output.txt == txt and output.tar.gz == tar.gz.
+	 *
+	 * @return the complete extension of the file.
+	 */
+	String completeExtension() const;
+
+	/**
+	 * Returns the filename of the file, i.e. output.txt == output.txt and output.tar.gz == output.tar.gz.
+	 *
+	 * @return the filename of the file.
+	 */
+	String filename() const;
+
 protected:
+
+	/**
+	 * Validates that the path is valid.
+	 *
+	 * Throws a bump::FileSystemError if the path is not valid.
+	 */
+	void _validatePath() const;
 
 	/** Instance member variables. */
 	boost::filesystem::path _path;
--- src/bump/FileInfo.cpp
+++ src/bump/FileInfo.cpp
@@ -10,6 +10,7 @@
 #include <boost/filesystem.hpp>
 
 // Bump headers
+#include <bump/Exception.h>
 #include <bump/FileInfo.h>
 
 using namespace bump;
@@ -28,6 +29,21 @@ FileInfo::~FileInfo()
 bool FileInfo::exists() const
 {
 	return boost::filesystem::exists(_path);
+}
+
+unsigned long long FileInfo::fileSize() const
+{
+	// Throw a FileSystemError if the path is not valid
+	_validatePath();
+
+	// Throw a FileSystemError if the path is not a file
+	if (!isFile())
+	{
+		String msg = String("The following path is not a file: %1").arg(_path.string());
+		throw FileSystemError(msg, BUMP_LOCATION);
+	}
+
+	return boost::filesystem::file_size(_path);
 }
 
 bool FileInfo::isAbsolute() const
@@ -55,6 +71,12 @@ bool FileInfo::isSymbolicLink() const
 	return boost::filesystem::is_symlink(_path);
 }
 
+bool FileInfo::isEmpty() const
+{
+	_validatePath();
+	return boost::filesystem::is_empty(_path);
+}
+
 bool FileInfo::isHidden() const
 {
 	// It is not a hidden file if it isn't even a file
@@ -65,4 +87,107 @@ bool FileInfo::isHidden() const
 	}
 
 	return false;
+}
+
+String FileInfo::absolutePath() const
+{
+	return boost::filesystem::absolute(_path).string();
+}
+
+String FileInfo::canonicalPath() const
+{
+	_validatePath();
+	return boost::filesystem::canonical(_path).string();
+}
+
+String FileInfo::parentPath() const
+{
+	return _path.parent_path().string();
+}
+
+String FileInfo::basename() const
+{
+	if (isDirectory())
+	{
+		return filename();
+	}
+	else
+	{
+		String basename = _path.stem().string();
+		if (basename.startsWith("."))
+		{
+			String temp = basename.right(basename.length() - 1);
+			if (temp.contains("."))
+			{
+				basename = basename.left(temp.indexOf(".") + 1);
+			}
+		}
+		else
+		{
+			if (basename.contains("."))
+			{
+				basename = basename.left(basename.indexOf("."));
+			}
+		}
+
+		return basename;
+	}
+}
+
+String FileInfo::completeBasename() const
+{
+	if (isDirectory())
+	{
+		return filename();
+	}
+	else
+	{
+		return _path.stem().string();
+	}
+}
+
+String FileInfo::extension() const
+{
+	if (isDirectory())
+	{
+		return String();
+	}
+	else
+	{
+		String extension = _path.extension().string();
+		if (extension.startsWith("."))
+		{
+			extension = extension.section(1);
+		}
+
+		return extension;
+	}
+}
+
+String FileInfo::completeExtension() const
+{
+	if (isDirectory())
+	{
+		return String();
+	}
+	else
+	{
+		String filename = this->filename();
+		String basename = this->basename() << ".";
+		return filename.remove(basename);
+	}
+}
+
+String FileInfo::filename() const
+{
+	return _path.filename().string();
+}
+
+void FileInfo::_validatePath() const
+{
+	if (!boost::filesystem::exists(_path))
+	{
+		String msg = String("The following path is invalid: %1").arg(_path.string());
+		throw FileSystemError(msg, BUMP_LOCATION);
+	}
 }
