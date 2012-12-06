44973905877011980907cda89114cd4eff1c3a1a
Christian Noon <christian.noon@gmail.com>
2012-12-05 18:26:29 -0600

    Added path query, directory, file and symbolic link methods to the FileSystem class.

--- include/bump/FileSystem.h
+++ include/bump/FileSystem.h
@@ -22,8 +22,10 @@ namespace bump {
  *
  *    - Join Paths (join, etc.)
  *    - System Paths (currentPath, setCurrentPath, temporaryPath, etc.)
+ *    - Path Queries (exists, isDirectory, isFile, isSymbolicLink, etc.)
  *    - Directories (createDirectory, removeDirectory, directoryInfoList, etc.)
  *    - Files (createFile, renameFile, removeFile, copyFile, etc.)
+ *    - Symbolic Links (createSymbolicLink, removeSymbolicLink, renameSymbolicLink, etc.)
  *    - Permissions (setIsReadableByUser, setIsExecutableByOwner, etc.)
  *    - Dates (setDateModified, etc.)
  */
@@ -163,6 +165,205 @@ extern BUMP_EXPORT String currentPath();
  * @return The temporary path for the operating system.
  */
 extern BUMP_EXPORT String temporaryPath();
+
+//====================================================================================
+//                               Path Query Methods
+//====================================================================================
+
+/**
+ * Returns whether the path exists.
+ *
+ * @param path The path to check if points to a valid file system object.
+ * @return True if the path exists, false otherwise.
+ */
+extern BUMP_EXPORT bool exists(const String& path);
+
+/**
+ * Returns whether the path points to a directory.
+ *
+ * @param path The path to check if points to a valid directory.
+ * @return True if the path points to a directory, false othwerwise.
+ */
+extern BUMP_EXPORT bool isDirectory(const String& path);
+
+/**
+ * Returns whether the path points to a file.
+ *
+ * @param path The path to check if points to a valid file.
+ * @return True if the path points to a file, false othwerwise.
+ */
+extern BUMP_EXPORT bool isFile(const String& path);
+
+/**
+ * Returns whether the path points to a symbolic link.
+ *
+ * @param path The path to check if points to a valid symbolic link.
+ * @return True if the path points to a symbolic link, false othwerwise.
+ */
+extern BUMP_EXPORT bool isSymbolicLink(const String& path);
+
+//====================================================================================
+//                                 Directory Methods
+//====================================================================================
+
+/**
+ * Creates the directory specified.
+ *
+ * @param path The path of the directory to create.
+ * @return True if the directory was created successfully, false otherwise.
+ */
+extern BUMP_EXPORT bool createDirectory(const String& path);
+
+/**
+ * Creates the full path for the directory specified.
+ *
+ * Each directory specified in the full path will be attempted to be created.
+ *
+ * @param path The path of the directory to create.
+ * @return True if the entire path was created successfully, false otherwise.
+ */
+extern BUMP_EXPORT bool createFullDirectoryPath(const String& path);
+
+/**
+ * Removes the specified directory if it is empty.
+ *
+ * @see removeDirectoryAndContents() can remove a directory that is not empty.
+ * @param path The path of the directory to remove.
+ * @return True if the directory was removed successfully, false otherwise.
+ */
+extern BUMP_EXPORT bool removeDirectory(const String& path);
+
+/**
+ * Removes the specified directory's contents recursively, then removes the directory itself.
+ *
+ * @param path The path of the directory to remove.
+ * @return True if the directory and it's contents were removed successfully, false otherwise.
+ */
+extern BUMP_EXPORT bool removeDirectoryAndContents(const String& path);
+
+/**
+ * Copies the source directory over to the destination directory.
+ *
+ * It is important to not that the contents of the source directory will not be copied
+ * to the destination directory using this method.
+ *
+ * @see copyDirectoryAndContents() can copy a directory along with all it's contents.
+ * @param source The source directory to copy.
+ * @param destination The destination directory to copy the source directory to.
+ * @return True if the source directory was copied successfully, false otherwise.
+ */
+extern BUMP_EXPORT bool copyDirectory(const String& source, const String& destination);
+
+/**
+ * Copies the source directory and all contents over to the destination directory.
+ *
+ * @param source The source directory to copy.
+ * @param destination The destination directory to copy the source directory to.
+ * @return True if the source directory and all contents were copied successfully, false otherwise.
+ */
+extern BUMP_EXPORT bool copyDirectoryAndContents(const String& source, const String& destination);
+
+/**
+ * Renames the source directory to the destination directory.
+ *
+ * @param source The source directory to rename.
+ * @param destination The destination directory to rename the source directory to.
+ * @return True if the source directory was renamed successfully, false otherwise.
+ */
+extern BUMP_EXPORT bool renameDirectory(const String& source, const String& destination);
+
+/**
+ * Creates a list of file system object paths contained within the directory.
+ *
+ * @param path The path of the directory.
+ * @return A string list of all the file system object paths contained within the directory.
+ */
+extern BUMP_EXPORT StringList directoryList(const String& path);
+
+/**
+ * Creates a list of FileInfo objects contained within the directory.
+ *
+ * @param path The path of the directory.
+ * @return A file info list of all the objects contained within the directory.
+ */
+extern BUMP_EXPORT FileInfoList directoryInfoList(const String& path);
+
+//====================================================================================
+//                                   File Methods
+//====================================================================================
+
+/**
+ * Creates the file specified.
+ *
+ * @param path The path of the file to create.
+ * @return True if the file was created successfully, false otherwise.
+ */
+extern BUMP_EXPORT bool createFile(const String& path);
+
+/**
+ * Removes the file specified.
+ *
+ * @param path The path of the file to remove.
+ * @return True if the file was removed successfully, false otherwise.
+ */
+extern BUMP_EXPORT bool removeFile(const String& path);
+
+/**
+ * Copies the source file over to the destination filepath.
+ *
+ * @param source The source file to copy.
+ * @param destination The destination file to copy the source file to.
+ * @return True if the source file was copied successfully, false otherwise.
+ */
+extern BUMP_EXPORT bool copyFile(const String& source, const String& destination);
+
+/**
+ * Renames the source file to the destination filepath.
+ *
+ * @param source The source file to rename.
+ * @param destination The destination file to rename the source file to.
+ * @return True if the source file was renamed successfully, false otherwise.
+ */
+extern BUMP_EXPORT bool renameFile(const String& source, const String& destination);
+
+//====================================================================================
+//                               Symbolic Link Methods
+//====================================================================================
+
+/**
+ * Creates the symbolic link from the specified source file or directory.
+ *
+ * @param source The path of the original file or directory.
+ * @param destination The path of the symbolic link to create.
+ * @return True if the symbolic link was created successfully, false otherwise.
+ */
+extern BUMP_EXPORT bool createSymbolicLink(const String& source, const String& destination);
+
+/**
+ * Removes the symbolic link.
+ *
+ * @param path The path of the symbolic link to remove.
+ * @return True if the symbolic link was removed successfully, false otherwise.
+ */
+extern BUMP_EXPORT bool removeSymbolicLink(const String& path);
+
+/**
+ * Copies the source symbolic link over over to the destination symbolic link.
+ *
+ * @param source The source symbolic link to copy.
+ * @param destination The destination symbolic link to copy the source symbolic link to.
+ * @return True if the source symbolic link was copied successfully, false otherwise.
+ */
+extern BUMP_EXPORT bool copySymbolicLink(const String& source, const String& destination);
+
+/**
+ * Renames the source symbolic link to the destination filepath.
+ *
+ * @param source The source symbolic link to rename.
+ * @param destination The destination symbolic link to rename the source symbolic link to.
+ * @return True if the source symbolic link was renamed successfully, false otherwise.
+ */
+extern BUMP_EXPORT bool renameSymbolicLink(const String& source, const String& destination);
 
 }	// End of FileSystem namespace
 
--- src/bump/FileSystem.cpp
+++ src/bump/FileSystem.cpp
@@ -8,10 +8,15 @@
 
 // Boost headers
 #include <boost/filesystem.hpp>
+#include <boost/foreach.hpp>
 
 // Bump headers
 #include <bump/Exception.h>
+#include <bump/FileInfo.h>
 #include <bump/FileSystem.h>
+
+// C++ headers
+#include <fstream>
 
 using namespace bump;
 
@@ -105,6 +110,10 @@ String FileSystem::join(const String& path1, const String& path2, const String& 
 	return joined_path.string();
 }
 
+//====================================================================================
+//                                System Path Methods
+//====================================================================================
+
 void FileSystem::setCurrentPath(const String& path)
 {
 	// Make sure the path is valid
@@ -127,4 +136,377 @@ String FileSystem::currentPath()
 String FileSystem::temporaryPath()
 {
 	return boost::filesystem::temp_directory_path().string();
+}
+
+//====================================================================================
+//                               Path Query Methods
+//====================================================================================
+
+bool FileSystem::exists(const String& path)
+{
+	FileInfo path_info(path);
+	return path_info.exists();
+}
+
+bool FileSystem::isDirectory(const String& path)
+{
+	FileInfo path_info(path);
+	return path_info.isDirectory();
+}
+
+bool FileSystem::isFile(const String& path)
+{
+	FileInfo path_info(path);
+	return path_info.isFile();
+}
+
+bool FileSystem::isSymbolicLink(const String& path)
+{
+	FileInfo path_info(path);
+	return path_info.isSymbolicLink();
+}
+
+//====================================================================================
+//                                 Directory Methods
+//====================================================================================
+
+bool FileSystem::createDirectory(const String& path)
+{
+	return boost::filesystem::create_directory(boost::filesystem::path(path));
+}
+
+bool FileSystem::createFullDirectoryPath(const String& path)
+{
+	return boost::filesystem::create_directories(boost::filesystem::path(path));
+}
+
+bool FileSystem::removeDirectory(const String& path)
+{
+	try
+	{
+		return boost::filesystem::remove(boost::filesystem::path(path));
+	}
+	catch (boost::filesystem::filesystem_error& e)
+	{
+		return false;
+	}
+}
+
+bool FileSystem::removeDirectoryAndContents(const String& path)
+{
+	try
+	{
+		return boost::filesystem::remove_all(boost::filesystem::path(path));
+	}
+	catch (boost::filesystem::filesystem_error& e)
+	{
+		return false;
+	}
+}
+
+bool FileSystem::copyDirectory(const String& source, const String& destination)
+{
+	try
+	{
+		boost::filesystem::path source_path(source);
+		boost::filesystem::path destination_path(destination);
+		boost::filesystem::copy(source_path, destination_path);
+		return true;
+	}
+	catch (boost::filesystem::filesystem_error& e)
+	{
+		return false;
+	}
+}
+
+bool FileSystem::copyDirectoryAndContents(const String& source, const String& destination)
+{
+	// Create a FileInfo objects for the source and destination
+	FileInfo source_info(source);
+	FileInfo destination_info(destination);
+
+	// Make sure the source is a directory
+	if (!source_info.isDirectory())
+	{
+		return false;
+	}
+
+	// Make sure the source exists
+	if (!source_info.exists())
+	{
+		return false;
+	}
+
+	// Make sure the destination does not exist
+	if (destination_info.exists())
+	{
+		return false;
+	}
+
+	// Create the destination directory
+	bool created_destination_directory = FileSystem::createFullDirectoryPath(destination);
+	if (!created_destination_directory)
+	{
+		return false;
+	}
+
+	// Iterate through the local source directory
+	FileInfoList local_source_files = FileSystem::directoryInfoList(source);
+	BOOST_FOREACH(const FileInfo& local_source_file, local_source_files)
+	{
+		bool copied_successfully;
+
+		// Attempt to copy all types of file system objects
+		if (local_source_file.isDirectory())
+		{
+			// Create the local destination directory path
+			String filename = local_source_file.filename();
+			String local_destination_directory = FileSystem::join(destination, filename);
+
+			// Copy the local source directory to the local destination directory
+			copied_successfully = FileSystem::copyDirectoryAndContents(local_source_file.absolutePath(), local_destination_directory);
+		}
+		else if (local_source_file.isSymbolicLink())
+		{
+			// Create the local destination symbolic link
+			String filename = local_source_file.filename();
+			String local_destination_file = FileSystem::join(destination, filename);
+
+			// Copy the local source symbolic link to the local destination
+			copied_successfully = FileSystem::copySymbolicLink(local_source_file.absolutePath(), local_destination_file);
+		}
+		else // local_source_info.isFile()
+		{
+			// Create the local destination filepath
+			String filename = local_source_file.filename();
+			String local_destination_file = FileSystem::join(destination, filename);
+
+			// Copy the local source file to the local destination
+			copied_successfully = FileSystem::copyFile(local_source_file.absolutePath(), local_destination_file);
+		}
+
+		// Fail if we did not copy the file system object successfully
+		if (!copied_successfully)
+		{
+			return false;
+		}
+	}
+
+	return true;
+}
+
+bool FileSystem::renameDirectory(const String& source, const String& destination)
+{
+	try
+	{
+		boost::filesystem::path source_path(source);
+		boost::filesystem::path destination_path(destination);
+		boost::filesystem::rename(source_path, destination_path);
+		return true;
+	}
+	catch (boost::filesystem::filesystem_error& e)
+	{
+		return false;
+	}
+}
+
+StringList FileSystem::directoryList(const String& path)
+{
+	// Throw an exception if the path does not exist
+	if (!FileInfo(path).exists())
+	{
+		String msg = String("The following path is not valid: %1").arg(path);
+		throw FileSystemError(msg, BUMP_LOCATION);
+	}
+
+	// Throw an exception if the path is not a directory
+	if (!FileInfo(path).isDirectory())
+	{
+		String msg = String("The following path is not a directory: %1").arg(path);
+		throw FileSystemError(msg, BUMP_LOCATION);
+	}
+
+	// Create a path and two directory iterators
+	boost::filesystem::path directory_path(path);
+	boost::filesystem::directory_iterator iter(directory_path);
+	boost::filesystem::directory_iterator end_iter;
+
+	// Iterate through the directory collecting all the item paths as strings
+	StringSet directory_set;
+	BOOST_FOREACH(const boost::filesystem::path& item, std::make_pair(iter, end_iter))
+	{
+		directory_set.insert(item.string());
+	}
+
+	// Build a string list from the sorted string set
+	StringList directory_list;
+	BOOST_FOREACH(const String& item, directory_set)
+	{
+		directory_list.push_back(item);
+	}
+
+	return directory_list;
+}
+
+FileInfoList FileSystem::directoryInfoList(const String& path)
+{
+	// Throw an exception if the path does not exist
+	if (!FileInfo(path).exists())
+	{
+		String msg = String("The following path is not valid: %1").arg(path);
+		throw FileSystemError(msg, BUMP_LOCATION);
+	}
+
+	// Throw an exception if the path is not a directory
+	if (!FileInfo(path).isDirectory())
+	{
+		String msg = String("The following path is not a directory: %1").arg(path);
+		throw FileSystemError(msg, BUMP_LOCATION);
+	}
+
+	// Create a path and two directory iterators
+	boost::filesystem::path directory_path(path);
+	boost::filesystem::directory_iterator iter(directory_path);
+	boost::filesystem::directory_iterator end_iter;
+
+	// Iterate through the directory collecting all the item paths as strings
+	StringSet directory_set;
+	BOOST_FOREACH(const boost::filesystem::path& item, std::make_pair(iter, end_iter))
+	{
+		directory_set.insert(item.string());
+	}
+
+	// Build a file info list from the sorted string set
+	FileInfoList directory_list;
+	BOOST_FOREACH(const String& item, directory_set)
+	{
+		FileInfo item_info(item);
+		directory_list.push_back(item_info);
+	}
+
+	return directory_list;
+}
+
+//====================================================================================
+//                                   File Methods
+//====================================================================================
+
+bool FileSystem::createFile(const String& path)
+{
+	// Make sure the file doesn't already exist
+	FileInfo path_info(path);
+	if (path_info.exists())
+	{
+		return false;
+	}
+
+	// Create the file and close it
+	std::ofstream stream(path.c_str());
+	stream.close();
+
+	return true;
+}
+
+bool FileSystem::removeFile(const String& path)
+{
+	return boost::filesystem::remove(boost::filesystem::path(path));
+}
+
+bool FileSystem::copyFile(const String& source, const String& destination)
+{
+	try
+	{
+		boost::filesystem::path source_path(source);
+		boost::filesystem::path destination_path(destination);
+		boost::filesystem::copy(source_path, destination_path);
+		return true;
+	}
+	catch (boost::filesystem::filesystem_error& e)
+	{
+		return false;
+	}
+}
+
+bool FileSystem::renameFile(const String& source, const String& destination)
+{
+	try
+	{
+		boost::filesystem::path source_path(source);
+		boost::filesystem::path destination_path(destination);
+		boost::filesystem::rename(source_path, destination_path);
+		return true;
+	}
+	catch (boost::filesystem::filesystem_error& e)
+	{
+		return false;
+	}
+}
+
+//====================================================================================
+//                               Symbolic Link Methods
+//====================================================================================
+
+bool FileSystem::createSymbolicLink(const String& source, const String& destination)
+{
+	try
+	{
+		boost::filesystem::path source_path(source);
+		boost::filesystem::path destination_path(destination);
+		if (boost::filesystem::is_directory(source_path))
+		{
+			boost::filesystem::create_directory_symlink(source_path, destination_path);
+		}
+		else
+		{
+			boost::filesystem::create_symlink(source_path, destination_path);
+		}
+
+		return true;
+	}
+	catch (boost::filesystem::filesystem_error& e)
+	{
+		return false;
+	}
+}
+
+bool FileSystem::removeSymbolicLink(const String& path)
+{
+	try
+	{
+		return boost::filesystem::remove(boost::filesystem::path(path));
+	}
+	catch (boost::filesystem::filesystem_error& e)
+	{
+		return false;
+	}
+}
+
+bool FileSystem::copySymbolicLink(const String& source, const String& destination)
+{
+	try
+	{
+		boost::filesystem::path source_path(source);
+		boost::filesystem::path destination_path(destination);
+		boost::filesystem::copy_symlink(source_path, destination_path);
+		return true;
+	}
+	catch (boost::filesystem::filesystem_error& e)
+	{
+		return false;
+	}
+}
+
+bool FileSystem::renameSymbolicLink(const String& source, const String& destination)
+{
+	try
+	{
+		boost::filesystem::path source_path(source);
+		boost::filesystem::path destination_path(destination);
+		boost::filesystem::rename(source_path, destination_path);
+		return true;
+	}
+	catch (boost::filesystem::filesystem_error& e)
+	{
+		return false;
+	}
 }
