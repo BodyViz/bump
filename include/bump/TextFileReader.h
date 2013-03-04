0b249872fb3962c3a98358548186b51b6be1062c
Joseph Holub <jholub@iastate.edu>
2013-03-04 10:10:31 -0600

    Updated TextFileReader to return StringLists instead of boost pointers. Need to test if this is faster.
    Changed a bunch of formatting in TextFileReader so that it matches the formatting in the rest of bump. Also removed the boost shared pointers int he code. Now we are returning bump objects like StringLists and Strings. This will need to be tested in the future to see which way is faster. This way is simpler if nothing else.

--- include/bump/TextFileReader.h
+++ include/bump/TextFileReader.h
@@ -12,7 +12,6 @@
 
 #include <bump/Export.h>
 #include <bump/String.h>
-#include <boost/shared_ptr.hpp>
 
 namespace bump {
 
@@ -27,29 +26,25 @@ namespace bump {
 namespace TextFileReader {
 	
 	/* Read entire file and place in StringList and return pointer or ref */
-    BUMP_EXPORT boost::shared_ptr<StringList> fileContents(const String fileName) ;
+    BUMP_EXPORT StringList fileContents(const String& fileName) ;
     
 	/* Read first line of the file and return String */
-    BUMP_EXPORT bump::String firstLine(const String fileName) ;
+    BUMP_EXPORT String firstLine(const String& fileName) ;
     
 	/* Read first numLines number of lines of a file and return */
-    BUMP_EXPORT boost::shared_ptr<StringList> header(const String fileName, const int numLines);
+    BUMP_EXPORT StringList header(const String& fileName, const int& numLines);
+	
+	/* Read the last numLines number of lines of the file */
+	BUMP_EXPORT StringList footer(const String& fileName, const int& numLines);
     
 	/* Read numLines number of lines starting at line begginningLine (lines start at number 0) */
-    BUMP_EXPORT boost::shared_ptr<StringList> fileContents(const String fileName, const int beginningLine, const int numLines);
+    BUMP_EXPORT StringList fileContents(const String& fileName, const int& beginningLine, const int& numLines);
 	
 	/* Read the rest of the file starting at line begginningLine through the end of the file */
-	BUMP_EXPORT boost::shared_ptr<StringList> fileContents(const String fileName, const int beginningLine);
+	BUMP_EXPORT StringList fileContents(const String& fileName, const int& beginningLine);
 	
 	/* Returns the number of lines in the file or -1 if there was an error */
-	BUMP_EXPORT int numberOfLines(const String fileName);
-	
-	//TODO: Read the footer of the File
-	
-    
-    
-    /* Reads from the file at the beginning line for the number of lines (numLines = -1 for the entire file) */
-    //static boost::shared_ptr<bump::StringList> readFileLines(const bump::String, const int beginningLine, const int numLines);
+	BUMP_EXPORT int numberOfLines(const String& fileName);
     
 }	// End of TextFileReader namespace
 	
--- src/bump/TextFileReader.cpp
+++ src/bump/TextFileReader.cpp
@@ -16,137 +16,123 @@ namespace bump {
 	
 namespace TextFileReader{
 	
-boost::shared_ptr<StringList> readFileLines(String fileName, int beginningLine, int numLines){
+StringList readFileLines(String fileName, int beginningLine, int numLines){
 	
 	// Create StringList to store info
-	boost::shared_ptr<StringList> fileContents(new StringList);
+	StringList file_contents;
 	
 	// Check to see if the file is valid before opening
-	bool isValid = FileSystem::isFile(fileName);
-	if(!isValid){
+	bool is_valid = FileSystem::isFile(fileName);
+	if(!is_valid){
 		bumpALWAYS_P("FileSystem: ", "File to open is not a valid file");
-		return fileContents;
+		return file_contents;
 	}
 	bumpINFO_P("FileReader: Reading File ", fileName);
 	
 	// Open File
-	std::ifstream inputFile;
-	inputFile.open(fileName.toStdString().data(), std::ios::in);
-	if (!inputFile.is_open()) {
+	std::ifstream input_file;
+	input_file.open(fileName.toStdString().data(), std::ios::in);
+	if (!input_file.is_open()) {
 		bumpALWAYS_P("FileReader: Error opening ", fileName);
-		return fileContents;
+		return file_contents;
 	}
 	
 	std::string line;
-	inputFile.seekg(0, std::ios::beg);
+	input_file.seekg(0, std::ios::beg);
 	for (unsigned int i = 1; i < beginningLine; i++) {
-		std::getline(inputFile, line);
-		if (inputFile.eof()) {
+		std::getline(input_file, line);
+		if (input_file.eof()) {
 			bumpALWAYS_P("FileReader: ", "The line requested is larger than the number of lines in the file");
-			return fileContents;
+			return file_contents;
 		}
 	}
 	
 	if (numLines == -1) {
-		while (!inputFile.eof()) {
-			std::getline(inputFile, line);
-			fileContents->push_back(line);
+		while (!input_file.eof()) {
+			std::getline(input_file, line);
+			file_contents.push_back(line);
 		}
 	}else{
 		// Read the specified lines from the file
 		for (unsigned int i = 0; i < numLines; i++) {
-			if (inputFile.eof()) {
+			if (input_file.eof()) {
 				bumpINFO_P("FileReader: ", "More lines were requested than were in the file");
-				return fileContents;
+				return file_contents;
 			}
-			std::getline(inputFile, line);
-			fileContents->push_back(line);
+			std::getline(input_file, line);
+			file_contents.push_back(line);
 		}
 	}
 	
 	
-	inputFile.close();
+	input_file.close();
 	
-	return fileContents;
+	return file_contents;
 	
 }
 
-boost::shared_ptr<StringList> returnFileContents(String fileName){
+StringList returnFileContents(const String& fileName){
 	
-	boost::shared_ptr<StringList> fileContents(new StringList);
-	fileContents = readFileLines(fileName, 0, -1); // -1 for the whole file
-	
-	return fileContents;
+	return readFileLines(fileName, 0, -1); // -1 for the whole file
 }
 
-String returnFirstLine(String fileName){
+String returnFirstLine(const String& fileName){
 	
-	boost::shared_ptr<StringList> fileContents(new StringList);
-	fileContents = readFileLines(fileName, 0, 1);
+	StringList file_contents = readFileLines(fileName, 0, 1);
 	
-	if(fileContents->empty()){
-		bump::String emptyString;
-		return emptyString;
+	String header;
+	if(!file_contents.empty()){
+		header = file_contents.front();
 	}
 	
-	return fileContents->front();
+	return header;
 }
 
-boost::shared_ptr<StringList> returnFirstNLines(String fileName, int numLines){
+StringList returnFirstNLines(const String& fileName, const int& numLines){
 	
-	boost::shared_ptr<StringList> fileContents(new StringList);
-	fileContents = readFileLines(fileName, 0, numLines);
-	
-	return fileContents;
+	return readFileLines(fileName, 0, numLines);
 }
 
-boost::shared_ptr<StringList> returnNLinesStartingAtM(String fileName, int beginningLine, int numLines){
+StringList returnNLinesStartingAtM(const String& fileName, const int& beginningLine, const int& numLines){
 	
-	boost::shared_ptr<StringList> fileContents(new StringList);
-	fileContents = readFileLines(fileName, beginningLine, numLines);
-	
-	return fileContents;
+	return readFileLines(fileName, beginningLine, numLines);
 }
 
-boost::shared_ptr<StringList> returnLineNThroughEnd(String fileName, int beginningLine){
+StringList returnLineNThroughEnd(const String& fileName, const int& beginningLine){
 	
-	int numberOfLines = -1; // -1 for the whole file
-	
-	boost::shared_ptr<StringList> fileContents(new StringList);
-	fileContents = readFileLines(fileName, beginningLine, numberOfLines);
-	
-	return fileContents;
+	int number_of_lines = -1; // -1 for the rest of the file
+	return readFileLines(fileName, beginningLine, number_of_lines);
 }
 
-int numberOfLines(String fileName){
+int numberOfLines(const String& fileName){
 	
 	// Check to see if the file is valid before opening
-	bool isValid = FileSystem::isFile(fileName);
-	if(!isValid){
+	bool is_valid = FileSystem::isFile(fileName);
+	if(!is_valid){
 		bumpALWAYS_P("FileSystem: ", "File to open is not a valid file");
 		return -1;
 	}
 	bumpINFO_P("FileReader: Reading File ", fileName);
 	
 	// Open File
-	std::ifstream inputFile;
-	inputFile.open(fileName.toStdString().data(), std::ios::in);
-	if (!inputFile.is_open()) {
+	std::ifstream input_file;
+	input_file.open(fileName.toStdString().data(), std::ios::in);
+	if (!input_file.is_open()) {
 		bumpALWAYS_P("FileReader: Error opening ", fileName);
 		return -1;
 	}
 	
 	std::string line;
-	inputFile.seekg(0, std::ios::beg);
-	unsigned int numLines = 0;
-	while (!inputFile.eof()) {
-		std::getline(inputFile, line);
-		numLines++;
+	input_file.seekg(0, std::ios::beg);
+	unsigned int num_lines = 0;
+	while (!input_file.eof()) {
+		std::getline(input_file, line);
+		num_lines++;
 	}
 	
-	inputFile.close();
+	input_file.close();
 	
-	return numLines;
+	return num_lines;
 }
 
 
